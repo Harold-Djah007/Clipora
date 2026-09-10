e555834104e8fcba14554147ac401ffea7a0edf8
Harold-Djah007 <146589784+Harold-Djah007@users.noreply.github.com>
2026-09-10 13:51:35 +0000

    Align Android namespace with Clipora package

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -28,7 +28,7 @@ val cliporaResolverUrl = (project.findProperty("CLIPORA_RESOLVER_URL") as String
     ?: ""
 
 android {
-    namespace = "com.threadvault.threadvault"
+    namespace = "com.threadvault.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -43,7 +43,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.threadvault.threadvault"
+        applicationId = "com.threadvault.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
